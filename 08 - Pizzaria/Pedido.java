e6ce2c5db31c5ea8a60220d15744915d12a923a1
Matheus Santos <142417170+theusroma@users.noreply.github.com>
2025-04-01 15:35:18 -0300

    Update Pedido.java

--- 08 - Pizzaria/Pedido.java
+++ 08 - Pizzaria/Pedido.java
@@ -2,18 +2,19 @@ import java.util.ArrayList;
 
 public class Pedido {
     private ArrayList<Pizza> pizzas;
-    private ArrayList<Pizza> menu;
-
+    private ArrayList<PedidoItem> menu;
+    private ArrayList<PedidoItem> registros;
 
     public Pedido(){
         pizzas = new ArrayList<>();
         menu = new ArrayList<>();
+        registros = new ArrayList<>();
+
         // Adicionando sabores padrão automaticamente
         pizzas.add(new Pizza("Margarita", 30.0f, "Molho de tomate, queijo, manjericão"));
         pizzas.add(new Pizza("Calabresa", 35.0f, "Molho de tomate, queijo, calabresa"));
         pizzas.add(new Pizza("Portuguesa", 40.0f, "Molho de tomate, queijo, presunto, ovo, azeitona"));
         pizzas.add(new Pizza("Quatro Queijos", 45.0f, "Molho de tomate, queijo mussarela, queijo prato, queijo parmesão, queijo gorgonzola"));
-
     }
 
     public void cardapio(){
@@ -27,11 +28,13 @@ public class Pedido {
         System.out.println();
     }
 
-    public void adicionarPedido(String nome) {
+    public void adicionarPedido(String nome, String tamanho, String endereco) {
         for (Pizza p : pizzas) { // Verifica se a pizza está no cardápio
             if (p.getNome().equalsIgnoreCase(nome)) {
-                menu.add(p); // Adiciona ao carrinho
-                System.out.println("Pedido de pizza adicionado: " + nome);
+                PedidoItem pedido = new PedidoItem(p, tamanho, endereco);
+                menu.add(pedido); // Adiciona ao carrinho
+                registros.add(pedido); // Adiciona no registro
+                System.out.println("Pedido adicionado com sucesso!");
                 return;
             }
         }
@@ -45,19 +48,18 @@ public class Pedido {
             System.out.println("Não há pizzas no carrinho.");
         } else {
             float total = 0;
-            for (Pizza p : menu) {
-                System.out.println("Nome: " + p.getNome() + " | Valor: " + p.getValor());
-                total += p.getValor();
+            for (PedidoItem p : menu) {
+                p.exibirPedido();
+                total += p.getPizza().getValor();
             }
             System.out.println("Total: " + total);
             System.out.println();
         }
     }
 
-
     public void cancelarPedido(String nome){
-        for (Pizza p : menu) {
-            if (p.getNome().equalsIgnoreCase(nome)) {
+        for (PedidoItem p : menu) {
+            if (p.getPizza().getNome().equalsIgnoreCase(nome)) {
                 menu.remove(p);
                 System.out.println("Pizza " + nome + " removida do carrinho.");
                 return;
@@ -66,29 +68,25 @@ public class Pedido {
         System.out.println("Pedido não encontrado.");
     }
 
-
     public void pedidosRegistrados(){
-        if (menu.isEmpty()) {
-            System.out.println("Nenhuma pizza foi registrada até o momento");
+        if (registros.isEmpty()) {
+            System.out.println("Nenhum pedido registrado.");
         } else {
             System.out.println("Pedidos registrados:");
-            for (Pizza p : menu){
-                p.exibirDados();
+            float total = 0;
+            for (PedidoItem p : registros){
+                p.exibirPedido();
+                total += p.getPizza().getValor();
             }
+            System.out.println("Total de pedidos: " + registros.size());
+            System.out.println("Média de preço: " + (total / registros.size()));
         }
         System.out.println();
     }
 
-
-    // Método para cadastrar um novo sabor de pizza no cardápio.
-
     public void cadastrarSabor(String nome, float valor, String ingrediente){
-
-        pizzas.add(new Pizza(nome, valor, ingrediente));  // Adiciona uma nova pizza ao ArrayList.
-        System.out.println("Sabor cadastrado com sucesso!"); // Confirma o cadastro ao usuário.
+        pizzas.add(new Pizza(nome, valor, ingrediente));
+        System.out.println("Sabor cadastrado com sucesso!");
         System.out.println();
-
     }
-
-
 }
